86602bacb42c5dab9ccfd567d124eba3be1f678c
Payal Jadakar <payal.jadakar@capgemini.com>
2022-09-13 10:09:36 +0530

    Add project files.

---
+++ 13sep/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _13sep
+{
+
+    //program for multicast  deligate...
+
+    class Program
+    {
+        public delegate void Shape(int length, int height);
+        public void show(int l, int h)
+        {
+            int area = l * h;
+            Console.WriteLine("Area is " + area);
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Shape s = new Shape(p.show);
+            s.Invoke(3, 4);
+        }
+
+    }
+}
+
